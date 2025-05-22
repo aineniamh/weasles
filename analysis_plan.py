35b2376db980eaebb116d16ad5864c3d3810e87d
aineniamh <aine.otoole@ed.ac.uk>
2025-05-22 12:47:10 +0100

    initial commit & plan for analytic solns

---
+++ analysis_plan.py
@@ -0,0 +1,11 @@
+"""
+alignment
+curation- does it need masking
+phylogenetics- iqtree, timetree? 
+report for outbreak investigation, but specifically for measles
+maps of area of interest
+imports- teasing apart what you can say about the missing data
+tricks to incorporate both the whole genome 
+and short fragment data to inform analysis and inference
+
+"""
